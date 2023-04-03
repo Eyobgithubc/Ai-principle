0b30c4bec04db555c6981c4e36b64643c4bc0e25
Eyobgithubc <105054977+Eyobgithubc@users.noreply.github.com>
2023-04-03 10:00:41 +0300

    Update buyLotsOfFruit.py

--- tutorial/buyLotsOfFruit.py
+++ tutorial/buyLotsOfFruit.py
@@ -35,7 +35,11 @@ def buyLotsOfFruit(orderList):
     Returns cost of order
     """
     totalCost = 0.0
-    "*** YOUR CODE HERE ***"
+    for fruit,pounds in orderList:
+        if fruit not in fruitPrices:
+            print(fruit+"is not in the list")
+            return None
+        totalCost+=fruitPrices[fruit]*pounds
     return totalCost
 
 
